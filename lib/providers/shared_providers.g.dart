8b7160a8764cb9f57e1bb6e8679c4d70a3d8d894
samuelnihoul <samuelnihoul@ecomail.bzh>
2024-07-15 18:10:45 +0200

    floating button

---
+++ lib/providers/shared_providers.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'shared_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$mySQLErrorHash() => r'92d38b9ce2cd43701da24b453f40f9bf2743b988';
+
+/// See also [MySQLError].
+@ProviderFor(MySQLError)
+final mySQLErrorProvider =
+    AutoDisposeNotifierProvider<MySQLError, Error>.internal(
+  MySQLError.new,
+  name: r'mySQLErrorProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$mySQLErrorHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MySQLError = AutoDisposeNotifier<Error>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
